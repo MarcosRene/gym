ff967f3463dbd6343af995113db7496d14b3b7eb
Marcos Renê <renemoura789@gmail.com>
2024-03-19 12:29:50 -0300

    feat: add exercise header

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,45 @@
-import { Center, Text } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { HStack, Heading, Icon, Text, VStack } from 'native-base';
+import { Feather } from '@expo/vector-icons';
+
+import { AppNavigatorRoutesProps } from '@/routes/app.routes';
+
+import BodySvg from '@/assets/body.svg';
 
 export function Exercise() {
+  const { goBack } = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    goBack();
+  }
+
   return (
-    <Center flex={1} bg="gray.700">
-      <Text color="white">Exercise</Text>
-    </Center>
+    <VStack flex={1} bg="gray.700">
+      <VStack px={8} pt={12} bg="gray.600">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+
+        <HStack
+          mt={4}
+          mb={8}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading fontSize="lg" color="gray.100" flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text ml={1} color="gray.200" textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
 
 import { ExerciseCard } from '@/components/ExerciseCard';
 import { Group } from '@/components/Group';
 import { HomeHeader } from '@/components/HomeHeader';
 
+import { AppNavigatorRoutesProps } from '@/routes/app.routes';
+
 export function Home() {
+  const { navigate } = useNavigation<AppNavigatorRoutesProps>();
+
   const [groups, setGroups] = useState<string[]>([
     'costas',
     'ombros',
@@ -21,6 +26,10 @@ export function Home() {
     'levantamento terra',
   ]);
   const [groupSelected, setGroupSelected] = useState('costas');
+
+  function handleOpenExerciseDetails() {
+    navigate('exercise');
+  }
 
   return (
     <VStack flex={1} bg="gray.700">
@@ -57,7 +66,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{
             paddingBottom: 20,
