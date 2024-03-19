169a28bfe9e3b8889f79987fe18f29b6b21226d0
Marcos Renê <renemoura789@gmail.com>
2024-03-19 12:02:47 -0300

    feat: add photo skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,37 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { Center, ScrollView, Skeleton, VStack } from 'native-base';
+
+import { ScreenHeader } from '@/components/ScreenHeader';
+import { UserPhoto } from '@/components/UserPhoto';
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
-    <Center flex={1} bg="gray.700">
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1} bg="gray.700">
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/MarcosRene.png' }}
+              alt="Imagem do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
