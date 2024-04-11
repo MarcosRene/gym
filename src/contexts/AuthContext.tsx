195df0a11fa6d7dc7a47ac2750cfe0f1d8853140
Marcos Rene <renemoura789@gmail.com>
2024-04-11 14:40:28 -0300

    feat: add token management

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -113,6 +113,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     loadUserData();
   }, []);
 
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut);
+
+    return () => subscribe();
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
